7685394c4b913f61841c51ba87c782a398e3b099
Arsen <tatarin08082002@gmail.com>
2021-09-06 01:14:15 +0300

    added section ndesign, mixitup and fancybox scripts

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,4 @@
+
 $(function () {
   $('.slider-inner').slick({
     dots: true,
@@ -6,6 +7,25 @@ $(function () {
     autoplay: true,
     autoplaySpeed: 2000
   });
-
-  var mixer = mixitup('.products__mix-items');
+  
+  var mixer = mixitup('.products__mix-items',{
+    animation: {
+      duration: 357,
+      nudge: true,
+      reverseOut: true,
+      effects: "fade scale(0.01) translateY(20%) rotateX(90deg)"
+    }
+  }).show();
+  
+  
+  var mixer2 = mixitup('.ndesign__mix-items',{
+    animation: {
+      duration: 357,
+      nudge: true,
+      reverseOut: true,
+      effects: "fade scale(0.01) translateY(20%) rotateX(90deg)"
+    }
+  }).show();
+  
 });
+
